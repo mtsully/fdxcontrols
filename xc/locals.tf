a8987aaab68a3aff66ff55878d22fbf2c26c5753
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-16 01:07:15 -0700

    Update locals.tf

--- xc/locals.tf
+++ xc/locals.tf
@@ -1,9 +1,8 @@
 locals {
   project_prefix = data.tfe_outputs.infra.values.project_prefix
   build_suffix = data.tfe_outputs.infra.values.build_suffix
-  origin_bigip = try(data.tfe_outputs.bigip.values.bigip_public_vip, "")
-  origin_nginx = try(data.tfe_outputs.nap.values.external_name, data.tfe_outputs.nic.values.external_name, "")
-  origin_server = "${coalesce(local.origin_bigip, local.origin_nginx)}"
-  origin_port = try(data.tfe_outputs.nap.values.external_port, data.tfe_outputs.nic.values.external_port, "80")
+  origin_nginx = try(data.tfe_outputs.nap.values.external_name, "")
+  origin_server = local.origin_nginx
+  origin_port = try(data.tfe_outputs.nap.values.external_port, "80")
   dns_origin_pool = local.origin_nginx != "" ? true : false 
 }

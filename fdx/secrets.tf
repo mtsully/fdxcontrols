f898a4384887688ecabe280460698dab459db704
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-15 23:59:46 -0700

    Update secrets.tf

--- fdx/secrets.tf
+++ fdx/secrets.tf
@@ -8,8 +8,10 @@ resource "kubernetes_secret" "fdx-docker-registry" {
     data = {
         ".dockerconfigjson" = jsonencode({
             auths = {
-                "${var.nginx_registry}" = {
-                    "username" = var.nginx_jwt
+                "docker-registry.financialdataexchange.org" = {
+                    "username" = var.fdx_dockerhub_username
+                    "password" = var.fdx_dockerhub_password
+                    "auth"     = base64encode("${var.fdx_dockerhub_username}:${var.fdx_dockerhub_password}"
                 }
             }
         })

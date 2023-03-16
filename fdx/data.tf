c8c699ab13731a9d0fdf1c425b1fcded0d9b93db
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-15 21:30:28 -0700

    Update data.tf

--- fdx/data.tf
+++ fdx/data.tf
@@ -6,6 +6,10 @@ data "tfe_outputs" "eks" {
   organization = var.tf_cloud_organization
   workspace = "eks"
 }
+data "tfe_outputs" "nap" {
+  organization = var.tf_cloud_organization
+  workspace = "nap"
+}
 data "aws_eks_cluster_auth" "auth" {
   name = data.tfe_outputs.eks.values.cluster_name
 }

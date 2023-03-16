4fa0e82cd4df18ff360bc6a1d317859d2de700d5
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-15 21:17:10 -0700

    Update main.tf

--- fdx/main.tf
+++ fdx/main.tf
@@ -1,14 +1,4 @@
 
-terraform {
-  required_version = ">= 0.14.0"  
-  required_providers {
-    volterra = {
-      source = "gavinbunney/kubectl"
-      version = ">= 1.7.0"
-    }
-  }
-}
-
 provider "aws" {
     region     = local.aws_region
 }

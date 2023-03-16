0534b3422ff0969dd68dd7c5f492361cbb16af2c
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-16 01:05:27 -0700

    Update data.tf

--- xc/data.tf
+++ xc/data.tf
@@ -2,15 +2,7 @@ data "tfe_outputs" "infra" {
   organization = var.tf_cloud_organization
   workspace = "infra"
 }
-data "tfe_outputs" "bigip" {
-  organization = var.tf_cloud_organization
-  workspace = "bigip"
-}
 data "tfe_outputs" "nap" {
   organization = var.tf_cloud_organization
   workspace = "nap"
-}
-data "tfe_outputs" "nic" {
-  organization = var.tf_cloud_organization
-  workspace = "nic"
 }

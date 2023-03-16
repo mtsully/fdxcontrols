e102a6deb769fd004041c321b9e50adb01c5c967
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-15 23:42:59 -0700

    Update variables.tf

--- fdx/variables.tf
+++ fdx/variables.tf
@@ -3,3 +3,12 @@ variable "tf_cloud_organization" {
   type = string
   description = "TF cloud org (Value set in TF cloud)"
 }
+#FDX DockerHub Username and password
+variable "fdx_dockerhub_username" {
+  type = string
+  description = "TF cloud org (Value set in TF cloud)"
+}
+variable "fdx_dockerhub_password" {
+  type = string
+  description = "TF cloud org (Value set in TF cloud)"
+}

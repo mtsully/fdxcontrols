ae2bfa511f84da24b65e9bbf20b1b75d47dec047
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-20 15:13:16 -0700

    Update outputs.tf

--- gcp-infra/outputs.tf
+++ gcp-infra/outputs.tf
@@ -7,3 +7,13 @@ output "project_id" {
   value       = var.project_id
   description = "GCloud Project ID"
 }
+
+output "vpc_name" {
+  value       = google_compute_network.vpc.name
+  description = "GCloud VPC/Network Name"
+}
+
+output "subnet_name" {
+  value       = google_compute_subnetwork.subnet.name
+  description = "GCloud Subnetwork Name"
+}

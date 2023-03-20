af6b283d5a1614e7978c74c3c8dbdca73b248125
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-20 15:16:42 -0700

    Update locals.tf

--- gke-cluster/locals.tf
+++ gke-cluster/locals.tf
@@ -3,5 +3,4 @@ locals {
   region = data.tfe_outputs.gcp-infra.values.region
   network_name    = data.tfe_outputs.gcp-infra.values.vpc_name
   subnet_name = data.tfe_outputs.gcp-infra.values.subnet_name
-  cluster_name = data.tfe_outputs.gcp-infra.values.cluster_primary_name
 }

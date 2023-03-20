96818e33cbb3fb6a22def8aad91a369fd018cd97
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-20 15:14:45 -0700

    Delete network.tf

--- gcp-infra/network.tf
+++
@@ -1,7 +0,0 @@
-# Subnet
-resource "google_compute_subnetwork" "subnet" {
-  name          = "${var.project_id}-subnet"
-  region        = var.region
-  network       = google_compute_network.vpc.name
-  ip_cidr_range = "10.10.0.0/24"
-}

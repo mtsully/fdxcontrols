255ec006dfdf1ddcd4d16c23ddababaac2647c4c
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-20 14:53:35 -0700

    Update outputs.tf

--- gke-cluster/outputs.tf
+++ gke-cluster/outputs.tf
@@ -7,3 +7,18 @@ output "kubernetes_cluster_host" {
   value       = google_container_cluster.primary.endpoint
   description = "GKE Cluster Host"
 }
+
+output "kubernetes_cluster_client_certificate" {
+  value       = google_container_cluster.primary.master_auth.0.client_certificate
+  description = "GKE Cluster Client Certificate"
+}
+
+output "kubernetes_cluster_client_key" {
+  value       = google_container_cluster.primary.master_auth.0.client_key
+  description = "GKE Cluster Client Key"
+}
+
+output "kubernetes_cluster_ca_certificate" {
+  value       = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
+  description = "GKE Cluster CA Certificate"
+}

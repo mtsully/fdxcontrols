1280d330f7053403dd99c17a90fcbe6aa7b683a9
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-20 19:12:31 -0700

    Update xc_apip.tf

--- xc/xc_apip.tf
+++ xc/xc_apip.tf
@@ -1,6 +1,7 @@
+#AWS: name = format("%s-api-def-%s", local.project_prefix, local.build_suffix)
 resource "volterra_api_definition" "api-def" {
   count = var.xc_api_pro ? 1 : 0
-  name = format("%s-api-def-%s", local.project_prefix, local.build_suffix)
+  name = format("%s-api-def-%s", local.project_id, local.region)
   namespace = var.xc_namespace 
   swagger_specs = var.xc_api_spec
 }

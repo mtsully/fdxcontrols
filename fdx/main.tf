c95a11ff64b38ba46e03bcf2f1538ee1412dc083
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-20 18:21:04 -0700

    Update main.tf

--- fdx/main.tf
+++ fdx/main.tf
@@ -1,22 +1,57 @@
 
-provider "aws" {
-    region     = local.aws_region
+##################################################
+#AWS
+##################################################
+#provider "aws" {
+#    region     = local.aws_region
+#}
+#provider "kubernetes" {
+#    host = local.host
+#    cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
+#    token = data.aws_eks_cluster_auth.auth.token
+#}
+#provider "helm" {
+#    kubernetes {
+#        host = local.host
+#        cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
+#        token = data.aws_eks_cluster_auth.auth.token  
+#    }
+#}
+#provider "kubectl" {
+#  host                   = local.host
+#  cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
+#  token                  = data.aws_eks_cluster_auth.auth.token
+#  load_config_file       = false
+#}
+
+#######################################
+#GCP
+#######################################
+provider "google" {
+  project = local.project_id
+  region  = local.region
 }
+
 provider "kubernetes" {
-    host = local.host
-    cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
-    token = data.aws_eks_cluster_auth.auth.token
+    host = local.kubernetes_cluster_host
+    client_certificate = base64decode(local.kubernetes_cluster_client_certificate)
+    client_key = base64decode(local.kubernetes_cluster_client_key)
+    cluster_ca_certificate = base64decode(local.kubernetes_cluster_ca_certificate)
 }
+
 provider "helm" {
     kubernetes {
-        host = local.host
-        cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
-        token = data.aws_eks_cluster_auth.auth.token  
+        host = local.kubernetes_cluster_host
+        client_certificate = base64decode(local.kubernetes_cluster_client_certificate)
+        client_key = base64decode(local.kubernetes_cluster_client_key)
+        cluster_ca_certificate = base64decode(local.kubernetes_cluster_ca_certificate) 
     }
 }
+
 provider "kubectl" {
-  host                   = local.host
-  cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
-  token                  = data.aws_eks_cluster_auth.auth.token
+  host                   = local.kubernetes_cluster_host
+  client_certificate = base64decode(local.kubernetes_cluster_client_certificate)
+  client_key = base64decode(local.kubernetes_cluster_client_key)
+  cluster_ca_certificate = base64decode(local.kubernetes_cluster_ca_certificate) 
   load_config_file       = false
 }

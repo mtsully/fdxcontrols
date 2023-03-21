afca17b1c49a6e9b38f54d7862c301fead6416ff
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-20 18:42:48 -0700

    Update data.tf

--- fdx/data.tf
+++ fdx/data.tf
@@ -16,3 +16,15 @@
 #data "aws_eks_cluster_auth" "auth" {
 #  name = data.tfe_outputs.eks.values.cluster_name
 #}
+
+###########################################
+#GCP
+###########################################
+data "tfe_outputs" "gcp-infra" {
+  organization = var.tf_cloud_organization
+  workspace = "gcp-infra"
+}
+data "tfe_outputs" "gke" {
+  organization = var.tf_cloud_organization
+  workspace = "gke"
+}

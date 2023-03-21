036c9e048235db51d8663912dbac8a008d3933e1
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-20 18:05:17 -0700

    Update nap.tf

--- nap/nap.tf
+++ nap/nap.tf
@@ -1,5 +1,6 @@
+#AWS: name = format("%s-nap-%s", local.project_prefix, local.build_suffix)
 resource "helm_release" "nginx-plus-ingress" {
-    name = format("%s-nap-%s", local.project_prefix, local.build_suffix)
+    name = format("%s-nap-%s", local.project_id, local.region)
     repository = "https://helm.nginx.com/stable"
     chart = "nginx-ingress"
     namespace = kubernetes_namespace.nginx-ingress.metadata[0].name

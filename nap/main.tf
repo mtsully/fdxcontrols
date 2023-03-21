9966351f9878fa88b427651ab72cfc69827c0750
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-20 17:59:33 -0700

    Update main.tf

--- nap/main.tf
+++ nap/main.tf
@@ -26,15 +26,17 @@ provider "google" {
 }
 
 provider "kubernetes" {
-    host = local.host
-    cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
-    token = data.aws_eks_cluster_auth.auth.token
+    host = local.kubernetes_cluster_host
+    client_certificate = base64decode(local.kubernetes_cluster_client_certificate)
+    client_key = base64decode(local.kubernetes_cluster_client_key)
+    cluster_ca_certificate = base64decode(local.kubernetes_cluster_ca_certificate )
 }
+
 provider "helm" {
     kubernetes {
-        host = local.host
-        client_certificate
-        client_key
-        cluster_ca_certificate = base64decode(local.cluster_ca_certificate) 
+        host = local.kubernetes_cluster_host
+        client_certificate = base64decode(local.kubernetes_cluster_client_certificate)
+        client_key = base64decode(local.kubernetes_cluster_client_key)
+        cluster_ca_certificate = base64decode(local.kubernetes_cluster_ca_certificate ) 
     }
 }

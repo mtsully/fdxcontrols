874397b9c70a075cdf573bda92b6b6f81102814c
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-21 14:41:13 -0700

    Update locals.tf

--- xc/locals.tf
+++ xc/locals.tf
@@ -16,6 +16,7 @@
 locals {
   project_id = data.tfe_outputs.gcp-infra.values.project_id
   region = data.tfe_outputs.gcp-infra.values.region
+  deployment_name = data.tfe_outputs.gcp-infra.values.deployment_name
   origin_nginx = try(data.tfe_outputs.nap.values.external_name, "")
   origin_server = local.origin_nginx
   origin_port = try(data.tfe_outputs.nap.values.external_port, "80")

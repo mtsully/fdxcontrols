5cd76e0356d9fbd1c0ce4db217325c8163fa057d
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-21 13:23:36 -0700

    Update vpc.tf

--- gcp-infra/vpc.tf
+++ gcp-infra/vpc.tf
@@ -1,5 +1,5 @@
 # VPC
 resource "google_compute_network" "vpc" {
-  name                    = "${var.project_id}-vpc"
+  name                    = "${var.project_id}-fdx-ref-impl-vpc"
   auto_create_subnetworks = "false"
 }

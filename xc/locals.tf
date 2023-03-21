e833b7b52882c386ffc73af6602db759421ccfa4
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-20 19:10:50 -0700

    Update locals.tf

--- xc/locals.tf
+++ xc/locals.tf
@@ -1,6 +1,21 @@
+################################################################
+#AWS
+################################################################
+#locals {
+#  project_prefix = data.tfe_outputs.infra.values.project_prefix
+#  build_suffix = data.tfe_outputs.infra.values.build_suffix
+#  origin_nginx = try(data.tfe_outputs.nap.values.external_name, "")
+#  origin_server = local.origin_nginx
+#  origin_port = try(data.tfe_outputs.nap.values.external_port, "80")
+#  dns_origin_pool = local.origin_nginx != "" ? true : false 
+#}
+
+##############################################################
+#GCP
+##############################################################
 locals {
-  project_prefix = data.tfe_outputs.infra.values.project_prefix
-  build_suffix = data.tfe_outputs.infra.values.build_suffix
+  project_id = data.tfe_outputs.gcp-infra.values.project_id
+  region = data.tfe_outputs.gcp-infra.values.region
   origin_nginx = try(data.tfe_outputs.nap.values.external_name, "")
   origin_server = local.origin_nginx
   origin_port = try(data.tfe_outputs.nap.values.external_port, "80")

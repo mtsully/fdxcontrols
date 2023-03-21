0fad020967a2e4d8190e8f482e1d24cba566acfd
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-21 13:24:33 -0700

    Update subnetwork.tf

--- gcp-infra/subnetwork.tf
+++ gcp-infra/subnetwork.tf
@@ -1,6 +1,6 @@
 # Subnet
 resource "google_compute_subnetwork" "subnet" {
-  name          = "${var.project_id}-subnet"
+  name          = "${var.project_id}-fdx-ref-impl-subnet"
   region        = var.region
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.10.0.0/24"

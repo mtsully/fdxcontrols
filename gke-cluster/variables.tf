f12a4cca8b1c71ff1ba4c4ff86acf51d3b064c72
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-20 18:49:45 -0700

    Update variables.tf

--- gke-cluster/variables.tf
+++ gke-cluster/variables.tf
@@ -1,3 +1,8 @@
+#TF Cloud
+variable "tf_cloud_organization" {
+  type = string
+  description = "TF cloud org (Value set in TF cloud)"
+}
 variable "gke_num_nodes" {
   default     = 2
   description = "number of gke nodes"

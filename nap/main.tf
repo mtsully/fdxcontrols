281e6dab2b53a90d4d32532a56ed45a0b834c247
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-21 16:21:25 -0700

    Update main.tf

--- nap/main.tf
+++ nap/main.tf
@@ -26,7 +26,7 @@ provider "google" {
 }
 
 provider "kubernetes" {
-    host = local.kubernetes_cluster_host
+    host = "https://${local.kubernetes_cluster_host}"
     client_certificate = base64decode(local.kubernetes_cluster_client_certificate)
     client_key = base64decode(local.kubernetes_cluster_client_key)
     cluster_ca_certificate = base64decode(local.kubernetes_cluster_ca_certificate)
@@ -34,7 +34,7 @@ provider "kubernetes" {
 
 provider "helm" {
     kubernetes {
-        host = local.kubernetes_cluster_host
+        host = "https://${local.kubernetes_cluster_host}"
         client_certificate = base64decode(local.kubernetes_cluster_client_certificate)
         client_key = base64decode(local.kubernetes_cluster_client_key)
         cluster_ca_certificate = base64decode(local.kubernetes_cluster_ca_certificate)  

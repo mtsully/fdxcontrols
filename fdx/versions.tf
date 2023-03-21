c5d1d42158fb44e23bf48632005ffbe130bcb892
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-20 18:23:23 -0700

    Update versions.tf

--- fdx/versions.tf
+++ fdx/versions.tf
@@ -1,6 +1,10 @@
 terraform {
   required_version = ">= 0.14.0"  
   required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.27.0"
+    }
     kubectl = {
       source = "gavinbunney/kubectl"
       version = ">= 1.14.0"

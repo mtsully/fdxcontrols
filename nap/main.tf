851551bce4df42df3d896849ec4a2ee61129d915
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-20 17:46:59 -0700

    Update main.tf

--- nap/main.tf
+++ nap/main.tf
@@ -1,3 +1,6 @@
+######################################
+#AWS
+######################################
 provider "aws" {
     region     = local.aws_region
 }
@@ -11,5 +14,23 @@ provider "helm" {
         host = local.host
         cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
         token = data.aws_eks_cluster_auth.auth.token  
+    }
+}
+#######################################
+#GCP
+#######################################
+
+
+provider "kubernetes" {
+    host = local.host
+    cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
+    token = data.aws_eks_cluster_auth.auth.token
+}
+provider "helm" {
+    kubernetes {
+        host = local.host
+        client_certificate
+        client_key
+        cluster_ca_certificate = base64decode(local.cluster_ca_certificate) 
     }
 }

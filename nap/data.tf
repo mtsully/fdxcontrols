f200f8a917e84e91f0bfb48e2cb72b1b7a1294b4
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-20 18:01:17 -0700

    Update data.tf

--- nap/data.tf
+++ nap/data.tf
@@ -6,9 +6,13 @@ data "tfe_outputs" "eks" {
   organization = var.tf_cloud_organization
   workspace = "eks"
 }
-data "aws_eks_cluster_auth" "auth" {
-  name = data.tfe_outputs.eks.values.cluster_name
-}
+#############################################
+#AWS
+############################################
+#data "aws_eks_cluster_auth" "auth" {
+#  name = data.tfe_outputs.eks.values.cluster_name
+#}
+############################################
 data "kubernetes_service_v1" "nginx-service" {
   metadata {
     name = format("%s-%s", helm_release.nginx-plus-ingress.name, helm_release.nginx-plus-ingress.chart)

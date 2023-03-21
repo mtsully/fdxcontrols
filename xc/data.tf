843df78cf19e5d5b819624beb90db256ef04bc21
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-20 19:08:42 -0700

    Update data.tf

--- xc/data.tf
+++ xc/data.tf
@@ -1,6 +1,6 @@
-data "tfe_outputs" "infra" {
+data "tfe_outputs" "gcp-infra" {
   organization = var.tf_cloud_organization
-  workspace = "infra"
+  workspace = "gcp-infra"
 }
 data "tfe_outputs" "nap" {
   organization = var.tf_cloud_organization

bd4658ad39ece5f4ff359c9ca0d17e49cc3bcffd
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-20 18:17:28 -0700

    Update locals.tf

--- fdx/locals.tf
+++ fdx/locals.tf
@@ -1,6 +1,22 @@
+##############################################################
+#AWS
+##############################################################
+#locals {
+#  aws_region = data.tfe_outputs.infra.values.aws_region
+#  host = data.tfe_outputs.eks.values.cluster_endpoint
+#  cluster_ca_certificate = data.tfe_outputs.eks.values.kubeconfig-certificate-authority-data
+#  cluster_name = data.tfe_outputs.eks.values.cluster_name
+#} 
+
+##############################################################
+#GCP
+##############################################################
 locals {
-  aws_region = data.tfe_outputs.infra.values.aws_region
-  host = data.tfe_outputs.eks.values.cluster_endpoint
-  cluster_ca_certificate = data.tfe_outputs.eks.values.kubeconfig-certificate-authority-data
-  cluster_name = data.tfe_outputs.eks.values.cluster_name
-} 
+  project_id = data.tfe_outputs.gcp-infra.values.project_id
+  region = data.tfe_outputs.gcp-infra.values.region
+  kubernetes_cluster_name = data.tfe_outputs.gcp-infra.values.kubernetes_cluster_name
+  kubernetes_cluster_host = data.tfe_outputs.gcp-infra.values.kubernetes_cluster_host
+  kubernetes_cluster_client_certificate = data.tfe_outputs.gcp-infra.values.kubernetes_cluster_client_certificate
+  kubernetes_cluster_client_key = data.tfe_outputs.gcp-infra.values.kubernetes_cluster_client_key
+  kubernetes_cluster_ca_certificate =  data.tfe_outputs.gcp-infra.values.kubernetes_cluster_ca_certificate
+}

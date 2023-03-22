f06feceb963ad9e3515ad870133951d14b9a1c6b
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-21 18:47:47 -0700

    Update ingress.tf

--- fdx/ingress.tf
+++ fdx/ingress.tf
@@ -1,25 +1,25 @@
-resource "kubernetes_ingress_v1" "fdx-ingress" {
-  wait_for_load_balancer = true
-  metadata {
-    name = "fdx-ingress"
-  }
-  spec {
-    ingress_class_name = "nginx"
-    rule {
-      host = data.tfe_outputs.nap.values.external_name
-      http {
-        path {
-          path = "/"
-          backend {
-            service {
-                name = kubernetes_service.fdxri.metadata.0.name
-                port {
-                    number = 8080
-                }
-            }
-          }
-        }
-      }
-    }
-  }
-}
+#resource "kubernetes_ingress_v1" "fdx-ingress" {
+#  wait_for_load_balancer = true
+#  metadata {
+#    name = "fdx-ingress"
+#  }
+#  spec {
+#    ingress_class_name = "nginx"
+#    rule {
+#      host = data.tfe_outputs.nap.values.external_name
+#      http {
+#        path {
+#          path = "/"
+#          backend {
+#            service {
+#                name = kubernetes_service.fdxri.metadata.0.name
+#                port {
+#                    number = 8080
+#                }
+#            }
+#          }
+#        }
+#      }
+#    }
+#  }
+#}

f4df2711de44b0d21d566a398004b2449bab60f9
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-21 19:43:36 -0700

    Update outputs.tf

--- nap/outputs.tf
+++ nap/outputs.tf
@@ -1,6 +1,11 @@
 output "external_name" {
     value = data.kubernetes_service_v1.nginx-service.status.0.load_balancer.0.ingress.0.hostname
 }
+
+output "external_ip" {
+    value = data.kubernetes_service_v1.nginx-service.status.0.load_balancer.0.ingress.0.ip
+}
+
 output "external_port" {
     value = data.kubernetes_service_v1.nginx-service.spec.0.port.0.port
 }

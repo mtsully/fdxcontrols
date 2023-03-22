08377aef1393ae612b33af1c349ee623850d4aa1
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-21 17:53:02 -0700

    Update outputs.tf

--- gke-cluster/outputs.tf
+++ gke-cluster/outputs.tf
@@ -22,3 +22,9 @@ output "kubernetes_cluster_ca_certificate" {
   value       = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
   description = "GKE Cluster CA Certificate"
 }
+
+output "kubernetes_cluster_access_token" {
+  value       = nonsensitive(data.google_client_config.provider.access_token)
+  description = "GKE Cluster Access Token"
+}
+

bfcd50a220ddf7f9e8b82e059440eeed0638dd94
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-22 12:45:24 -0700

    Update fdx.tf

--- fdx/fdx.tf
+++ fdx/fdx.tf
@@ -28,6 +28,14 @@ resource "kubernetes_deployment" "fdxri" {
           }
           image_pull_policy = "IfNotPresent"
         }
+        container {
+          name  = "postgres-container"
+          image = "docker-registry.financialdataexchange.org/fdxri_pgdb:latest"
+          port {
+            container_port = 5432
+          }
+          image_pull_policy = "IfNotPresent"
+        }
         image_pull_secrets {
           name = "fdx-docker-registry"
         }
@@ -57,61 +65,61 @@ resource "kubernetes_service" "fdxri" {
   }
 }
 
-resource "kubernetes_deployment" "postgres-container" {
-  metadata {
-    name = "postgres-container"
-    labels = {
-      app = "postgres-container"
-    }
-  }
-  spec {
-    replicas = 1
+#resource "kubernetes_deployment" "postgres-container" {
+#  metadata {
+#    name = "postgres-container"
+#    labels = {
+#      app = "postgres-container"
+#    }
+#  }
+#  spec {
+#    replicas = 1
+#
+#    selector {
+#      match_labels = {
+#        app = "postgres-container"
+#      }
+#    }
+#    template {
+#      metadata {
+#        labels = {
+#          app = "postgres-container"
+#        }
+#      }
+#      spec {
+#        container {
+#          name  = "postgres-container"
+#          image = "docker-registry.financialdataexchange.org/fdxri_pgdb:latest"
+#          port {
+#            container_port = 5432
+#          }
+#          image_pull_policy = "IfNotPresent"
+#        }
+#        image_pull_secrets {
+#          name = "fdx-docker-registry"
+#        }
+#      }
+#    }
+#  }
+#}
 
-    selector {
-      match_labels = {
-        app = "postgres-container"
-      }
-    }
-    template {
-      metadata {
-        labels = {
-          app = "postgres-container"
-        }
-      }
-      spec {
-        container {
-          name  = "postgres-container"
-          image = "docker-registry.financialdataexchange.org/fdxri_pgdb:latest"
-          port {
-            container_port = 5432
-          }
-          image_pull_policy = "IfNotPresent"
-        }
-        image_pull_secrets {
-          name = "fdx-docker-registry"
-        }
-      }
-    }
-  }
-}
-
-resource "kubernetes_service" "postgres" {
-  metadata {
-    name = "postgres"
-    labels = {
-      app = "postgres-container"
-      service = "postgres"
-    }
-  }
-  spec {
-    port {
-      protocol    = "TCP"
-      port        = 5432
-      target_port = "5432"
-    }
-    selector = {
-      app = "postgres-container"
-    }
-    type = "ClusterIP"
-  }
-}
+#resource "kubernetes_service" "postgres" {
+#  metadata {
+#    name = "postgres"
+#    labels = {
+#      app = "postgres-container"
+#      service = "postgres"
+#    }
+#  }
+#  spec {
+#    port {
+#      protocol    = "TCP"
+#      port        = 5432
+#      target_port = "5432"
+#    }
+#    selector = {
+#      app = "postgres-container"
+#    }
+#    type = "ClusterIP"
+#  }
+#}

f2ebc96bd2b13eb13758afb09ef7d7b47e03f9a6
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-22 17:02:58 -0700

    Update fdx.tf

--- fdx/fdx.tf
+++ fdx/fdx.tf
@@ -1,59 +1,44 @@
-resource "kubernetes_deployment" "fdxri" {
+resource "kubernetes_deployment" "fdx-gen-stub" {
   metadata {
-    name = "fdxri"
+    name = "fdx-gen-stub"
     labels = {
-      app = "fdxri"
+      app = "fdx-gen-stub"
     }
   }
   spec {
     replicas = 1
     selector {
       match_labels = {
-        app = "fdxri"
+        app = "fdx-gen-stub"
       }
     }
     template {
       metadata {
         labels = {
-          app = "fdxri"
+          app = "fdx-gen-stub"
         }
       }
       spec {
-        host_aliases {
-          ip = "127.0.0.1"
-          hostnames = ["postgres_container"]
-        }
         container {
-          name  = "fdxri"
-          image = "docker-registry.financialdataexchange.org/fdxri_dc_tomcat:latest"
+          name  = "fdx-gen-stub"
+          image = "registry.gitlab.com/f5-security-test/fdx-generated-stub-server:latest"
 
           port {
             container_port = 8080
           }
           image_pull_policy = "IfNotPresent"
         }
-        container {
-          name  = "postgres-container"
-          image = "docker-registry.financialdataexchange.org/fdxri_pgdb:latest"
-          port {
-            container_port = 5432
-          }
-          image_pull_policy = "IfNotPresent"
-        }
-        image_pull_secrets {
-          name = "fdx-docker-registry"
-        }
       }
     }
   }
 }
 
-resource "kubernetes_service" "fdxri" {
+resource "kubernetes_service" "fdx-gen-stub" {
   metadata {
-    name = "fdxri"
+    name = "fdx-gen-stub"
     labels = {
-      app = "fdxri"
-      service = "fdxri"
+      app = "fdx-gen-stub"
+      service = "fdx-gen-stub"
     }
   }
   spec {
@@ -63,11 +48,84 @@ resource "kubernetes_service" "fdxri" {
       target_port = "8080"
     }
     selector = {
-      app = "fdxri"
+      app = "fdx-gen-stub"
     }
     type = "ClusterIP"
   }
 }
+
+
+
+#resource "kubernetes_deployment" "fdxri" {
+#  metadata {
+#    name = "fdxri"
+#    labels = {
+#      app = "fdxri"
+#    }
+#  }
+#  spec {
+#    replicas = 1
+#    selector {
+#      match_labels = {
+#        app = "fdxri"
+#      }
+#    }
+#    template {
+#      metadata {
+#        labels = {
+#          app = "fdxri"
+#        }
+#      }
+#      spec {
+#        host_aliases {
+#          ip = "127.0.0.1"
+#          hostnames = ["postgres_container"]
+#        }
+#        container {
+#          name  = "fdxri"
+#          image = "docker-registry.financialdataexchange.org/fdxri_dc_tomcat:latest"
+#
+#          port {
+#            container_port = 8080
+#          }
+#          image_pull_policy = "IfNotPresent"
+#        }
+#        container {
+#          name  = "postgres-container"
+#          image = "docker-registry.financialdataexchange.org/fdxri_pgdb:latest"
+#          port {
+#            container_port = 5432
+#          }
+#          image_pull_policy = "IfNotPresent"
+#        }
+#        image_pull_secrets {
+#          name = "fdx-docker-registry"
+#        }
+#      }
+#    }
+#  }
+#}
+
+#resource "kubernetes_service" "fdxri" {
+#  metadata {
+#    name = "fdxri"
+#    labels = {
+#      app = "fdxri"
+#      service = "fdxri"
+#    }
+#  }
+#  spec {
+#    port {
+#      protocol    = "TCP"
+#      port        = 8080
+#      target_port = "8080"
+#    }
+#    selector = {
+#      app = "fdxri"
+#    }
+#    type = "ClusterIP"
+#  }
+#}
 
 #resource "kubernetes_deployment" "postgres-container" {
 #  metadata {

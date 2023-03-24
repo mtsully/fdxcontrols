2c9ee64b7c214ac7f76d68d834756136e233cade
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-24 01:21:16 -0700

    Update fdxri_bridge_service.tf

--- fdx/fdxri_bridge_service.tf
+++ fdx/fdxri_bridge_service.tf
@@ -1,14 +1,11 @@
-resource "kubectl_manifest" "fdxri_bridge_service" {
-    depends_on = [kubectl_manifest.nginx_policy]
-    yaml_body = <<YAML
-kind: Service
-metadata:
-  name: fdxri-bridge-service
-  namespace: nginx-ingress
-spec:
-  type: ExternalName
-  externalName: fdx-gen-stub.default.svc.cluster.local
-  ports:
-  - port: 8080
-YAML
+resource "kubernetes_service" "fdxri_bridge_service" {
+  metadata {
+    name = fdxri-bridge-service
+    namespace = nginx-ingress
+  }
+  spec {
+    externalName = fdx-gen-stub.default.svc.cluster.local
+    type = "ExternalName"
+  }
 }
+

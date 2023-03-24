5efa1fb0ec717f677d9355f9f681c3855c54f1c4
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-24 02:26:13 -0700

    Update fdxri_bridge_service.tf

--- fdx/fdxri_bridge_service.tf
+++ fdx/fdxri_bridge_service.tf
@@ -1,10 +1,10 @@
 resource "kubernetes_service" "fdxri_bridge_service" {
   metadata {
-    name = fdxri-bridge-service
-    namespace = nginx-ingress
+    name = "fdxri-bridge-service"
+    namespace = "nginx-ingress"
   }
   spec {
-    externalName = fdx-gen-stub.default.svc.cluster.local
+    externalName = "fdx-gen-stub.default.svc.cluster.local"
     type = "ExternalName"
   }
 }

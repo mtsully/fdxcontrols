e0d0803da660961acb48897fd934df30d4ace8c7
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-25 19:47:12 -0700

    Update variables.tf

--- gcp-storage-bucket/variables.tf
+++ gcp-storage-bucket/variables.tf
@@ -1,3 +1,9 @@
+#TF Cloud
+variable "tf_cloud_organization" {
+  type = string
+  description = "TF cloud org (Value set in TF cloud)"
+}
+
 variable "bucket_name" {
   type = string
   description = "GCP Storage Bucket Name"

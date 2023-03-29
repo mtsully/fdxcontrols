40868f376099d1867c9f375839d7ef2b17b504df
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-29 15:12:55 -0700

    Update nginx_virtual_server.tf

--- fdx/nginx_virtual_server.tf
+++ fdx/nginx_virtual_server.tf
@@ -23,26 +23,3 @@ spec:
       pass: fdx-gen-stub
 YAML
 }
-
-
-#resource "kubectl_manifest" "nginx_virtual_server" {
-#    depends_on = [kubectl_manifest.nginx_policy]
-#    yaml_body = <<YAML
-#apiVersion: k8s.nginx.org/v1
-#kind: VirtualServer
-#metadata:
-#  name: fdxri
-#spec:
-#  host: "fdx-ref-impl.sr.f5-cloud-demo.com"
-#  policies:
-#  - name: jwt-policy
-#  upstreams:
-#  - name: fdxri
-#    service: kubernetes_service.fdx-gen-stub.metadata.0.name
-#    port: 8080
-#  routes:
-#  - path: /
-#    action:
-#      pass: fdxri
-#YAML
-#}

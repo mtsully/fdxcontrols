ae6e008e7b0952820175f9af7a5ec3b91e11c906
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-29 21:24:55 -0700

    Create variables.tf

---
+++ api-security-tester/variables.tf
@@ -0,0 +1,67 @@
+variable "adn_name" {
+  type        = string
+  description = "ADN Name. Also used as a prefix in names of related resources."
+}
+
+variable "volterra_namespace_exists" {
+  type        = string
+  description = "Flag to create or use existing volterra namespace"
+  default     = false
+}
+
+variable "volterra_namespace" {
+  type        = string
+  description = "Volterra app namespace where the object will be created. This cannot be system or shared ns."
+}
+
+
+variable "app_domain" {
+  type        = string
+  description = "FQDN for the app. If you have delegated domain `prod.example.com`, then your app_domain can be `<app_name>.prod.example.com`"
+}
+
+variable "enable_hsts" {
+  type        = bool
+  description = "Flag to enable hsts for HTTPS loadbalancer"
+  default     = false
+}
+
+variable "enable_redirect" {
+  type        = bool
+  description = "Flag to enable http redirect to HTTPS loadbalancer"
+  default     = true
+}
+
+variable "js_script_delay" {
+  type        = number
+  description = "Javascript challenge delay in miliseconds"
+  default     = 5000
+}
+
+variable "js_cookie_expiry" {
+  type        = number
+  description = "Javascript cookie expiry time in seconds"
+  default     = 3600
+}
+
+variable "disable_js_challenge" {
+  type        = bool
+  description = "disable javascript challenge"
+  default     = false
+}
+
+variable "blocking" {
+  type        = bool
+  description = "Enable blocking mode for app_firewall"
+  default     = true
+}
+
+variable "robot_tests_url" {
+  type        = string
+  description = "Test suite to be executed by Robot Framework"
+}
+
+variable "nuclei_templates_url" {
+  type        = string
+  description = "Custom Nuclei templates to be downloaded and added to the template db"
+}

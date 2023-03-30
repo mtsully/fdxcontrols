07ca56f0d5c8d5ec3485aa03041b8ba2df3364f1
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-29 21:31:56 -0700

    Create main.tf

---
+++ api-security-tester/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    volterra = {
+      source = "volterraedge/volterra"
+      version = ">= 0.11.5"
+    }
+  }
+}
+
+provider "volterra" {
+  api_p12_file = var.api_p12_file
+  url          = var.api_url
+}

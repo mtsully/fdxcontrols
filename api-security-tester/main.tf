7583a7da1bd69eccef92d5a5179e32e3d4918024
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-29 21:33:40 -0700

    Update main.tf

--- api-security-tester/main.tf
+++ api-security-tester/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    volterra = {
-      source = "volterraedge/volterra"
-      version = ">= 0.11.5"
-    }
-  }
-}
-
 provider "volterra" {
-  api_p12_file = var.api_p12_file
   url          = var.api_url
 }

4685677fed2b4b30301b1251166ea24fc4ce7100
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-29 21:23:04 -0700

    Create locals.tf

---
+++ api-security-tester/locals.tf
@@ -0,0 +1,13 @@
+locals {
+  namespace = var.volterra_namespace_exists ? join("", data.volterra_namespace.this.*.name) : join("", volterra_namespace.this.*.name)
+  fdx_security_tester_manifest_content = templatefile(format("%s/manifest/fdx_security_tester.tpl", path.module), {
+    robot_tests_url           = indent(4, var.robot_tests_url)
+    nuclei_templates_url       = indent(4, var.nuclei_templates_url)
+  })
+  js_delay_list = var.disable_js_challenge ? [] : [
+    {
+      js_script_delay = var.js_script_delay
+      cookie_expiry   = var.js_cookie_expiry
+    }
+  ]
+}

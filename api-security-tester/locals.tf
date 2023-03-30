6f7fdbf4323cfad9960b55eca5550cfdd3de9268
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-29 23:08:13 -0700

    Update locals.tf

--- api-security-tester/locals.tf
+++ api-security-tester/locals.tf
@@ -2,8 +2,8 @@ locals {
   namespace = var.volterra_namespace_exists ? join("", data.volterra_namespace.this.*.name) : join("", volterra_namespace.this.*.name)
   gcp_storage_bucket_name = data.tfe_outputs.gcp-storage-bucket.name
   fdx_security_tester_manifest_content = templatefile(format("%s/manifest/api_security_tester.tftpl", path.module), {
-    aws_s3_bucket_name    = indent(4, var.aws_s3_bucket_name)
-    gcp_storage_bucket_name    = indent(4, var.gcp_storage_bucket_name)
+    aws_s3_bucket_name    = indent(4, "")
+    gcp_storage_bucket_name    = indent(4, local.gcp_storage_bucket_name)
   })
   js_delay_list = var.disable_js_challenge ? [] : [
     {

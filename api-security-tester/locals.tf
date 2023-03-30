46d05cd38e436381942bc50063821b802c8b5d8c
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-29 22:40:30 -0700

    Update locals.tf

--- api-security-tester/locals.tf
+++ api-security-tester/locals.tf
@@ -1,5 +1,6 @@
 locals {
   namespace = var.volterra_namespace_exists ? join("", data.volterra_namespace.this.*.name) : join("", volterra_namespace.this.*.name)
+  gcp_storage_bucket_url = 
   fdx_security_tester_manifest_content = templatefile(format("%s/manifest/api_security_tester.tpl", path.module), {
     robot_tests_url           = indent(4, var.robot_tests_url)
     nuclei_templates_url       = indent(4, var.nuclei_templates_url)

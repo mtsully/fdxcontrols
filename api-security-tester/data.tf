e8263cd53749670079284fe2250688dbdfa12760
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-29 23:06:43 -0700

    Update data.tf

--- api-security-tester/data.tf
+++ api-security-tester/data.tf
@@ -2,8 +2,3 @@ data "tfe_outputs" "gcp-storage-bucket" {
   organization = var.tf_cloud_organization
   workspace = "gcp-storage-bucket"
 }
-
-data "tfe_outputs" "gcp-storage-bucket" {
-  organization = var.tf_cloud_organization
-  workspace = "gcp-storage-bucket"
-}

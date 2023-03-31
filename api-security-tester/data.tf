fdf3d9439fb39a2a7bac3eb7c2a3e42acb2a3745
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-31 01:24:13 -0700

    Update data.tf

--- api-security-tester/data.tf
+++ api-security-tester/data.tf
@@ -1,4 +1,4 @@
-#data "tfe_outputs" "xc-vk8s" {
-#  organization = var.tf_cloud_organization
-#  workspace = "xc-vk8s"
-#}
+data "tfe_outputs" "xc-vk8s" {
+  organization = var.tf_cloud_organization
+  workspace = "xc-vk8s"
+}

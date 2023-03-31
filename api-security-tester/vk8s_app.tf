1aeb5f8def99a0e61f21b8eba0df2549bac67661
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-30 21:14:21 -0700

    Update vk8s_app.tf

--- api-security-tester/vk8s_app.tf
+++ api-security-tester/vk8s_app.tf
@@ -75,23 +75,35 @@ resource "time_sleep" "wait_k8s_server" {
   create_duration = "120s"
 }
 
-resource "null_resource" "apply_manifest" {
-  depends_on = [time_sleep.wait_k8s_server]
-  triggers = {
-    manifest_sha1 = sha1(local.fdx_security_tester_manifest_content)
-  }
-  provisioner "local-exec" {
-    command = "kubectl apply -f _output/fdx-security-tester.yaml"
-    environment = {
-      KUBECONFIG = format("%s/_output/fdx_security_tester_adn_vk8s_kubeconfig", path.root)
-    }
-  }
-  provisioner "local-exec" {
-    when    = destroy
-    command = "kubectl delete -f _output/fdx-security-tester.yaml --ignore-not-found=true"
-    environment = {
-      KUBECONFIG = format("%s/_output/fdx_security_tester_adn_vk8s_kubeconfig", path.root)
-    }
-    on_failure = continue
-  }
+provider "kubectl" {
+  host                   = var.eks_cluster_endpoint
+  cluster_ca_certificate = base64decode(var.eks_cluster_ca)
+  token                  = data.aws_eks_cluster_auth.main.token
+  load_config_file       = true
 }
+
+
+resource "kubectl_manifest" "apply_manifest" {
+    yaml_body = local.fdx_security_tester_manifest_content
+}
+
+#resource "null_resource" "apply_manifest" {
+#  depends_on = [time_sleep.wait_k8s_server]
+#  triggers = {
+#    manifest_sha1 = sha1(local.fdx_security_tester_manifest_content)
+#  }
+#  provisioner "local-exec" {
+#    command = "kubectl apply -f _output/fdx-security-tester.yaml"
+#    environment = {
+#      KUBECONFIG = format("%s/_output/fdx_security_tester_adn_vk8s_kubeconfig", path.root)
+#    }
+#  }
+#  provisioner "local-exec" {
+#    when    = destroy
+#    command = "kubectl delete -f _output/fdx-security-tester.yaml --ignore-not-found=true"
+#    environment = {
+#      KUBECONFIG = format("%s/_output/fdx_security_tester_adn_vk8s_kubeconfig", path.root)
+#    }
+#    on_failure = continue
+#  }
+#}

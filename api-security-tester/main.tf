4e41fa4660b76a39b76452d07799c8d29d1a8511
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-30 22:26:22 -0700

    Update main.tf

--- api-security-tester/main.tf
+++ api-security-tester/main.tf
@@ -1,3 +1,8 @@
 provider "volterra" {
   url          = var.api_url
 }
+
+  provider "kubectl" {
+# Will use the KUBE_LOAD_CONFIG_FILE env variable set in Terraform Cloud to "_output/xc_vk8s_kubeconfig" 
+  load_config_file       = true
+}

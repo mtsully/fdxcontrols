00a4e06d757c63168430714445ad081d96943aba
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-30 21:05:21 -0700

    Update versions.tf

--- api-security-tester/versions.tf
+++ api-security-tester/versions.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "volterraedge/volterra"
       version = ">= 0.11.5"
     }
+    kubectl = {
+      source = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
     local = ">= 2.0"
     null  = ">= 3.0"
   }

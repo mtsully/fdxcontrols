bcf0b9531c130fbfc0af06fe234739754b0818f6
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-31 01:37:11 -0700

    Update variables.tf

--- api-security-tester/variables.tf
+++ api-security-tester/variables.tf
@@ -4,23 +4,6 @@ variable "tf_cloud_organization" {
   description = "TF cloud org (Value set in TF cloud)"
 }
 
-variable "adn_name" {
-  type        = string
-  description = "ADN Name. Also used as a prefix in names of related resources."
-}
-
-variable "api_url" {
-  type        = string
-  description = "Your F5 XC tenant api url"
-}
-
-
-variable "volterra_namespace" {
-  type        = string
-  description = "Volterra app namespace where the object will be created. This cannot be system or shared ns."
-}
-
-
 variable "app_domain" {
   type        = string
   description = "FQDN for the app. If you have delegated domain `prod.example.com`, then your app_domain can be `<app_name>.prod.example.com`"

abb4dd0d74848db58e5ac166114a060d5469a5ce
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-31 01:06:22 -0700

    Update outputs.tf

--- xc-vk8s-cluster/outputs.tf
+++ xc-vk8s-cluster/outputs.tf
@@ -4,5 +4,13 @@ output "xc_namespace" {
 }
 output "xc_vk8s_kubeconfig" {
   description = "XC vk8s generated kubeconfig"
-  value       = volterra_api_credential.this.data
+  value       = yamldecode(volterra_api_credential.this.data)
+}
+
+output "xc_vk8s_host" {
+    value = yamldecode(volterra_api_credential.this.data).clusters[0].cluster.server)
+}
+
+output "xc_vk8s_ca_cert" {
+    value = file(yamldecode(volterra_api_credential.this.data).clusters[0].cluster.certificate-authority)
 }

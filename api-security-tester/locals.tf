0ac283bb8f0541a8792447ba88decb9178d4ed73
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-31 02:54:15 -0700

    Update locals.tf

--- api-security-tester/locals.tf
+++ api-security-tester/locals.tf
@@ -1,6 +1,6 @@
 locals {
-  namespace = data.tfe_outputs.xc-vk8s.xc_namespace
-  adn_name = data.tfe_outputs.xc-vk8s.adn_name
+  namespace = data.tfe_outputs.xc-vk8s.values.xc_namespace
+  adn_name = data.tfe_outputs.xc-vk8s.values.adn_name
   fdx_security_tester_manifest_content = templatefile(format("%s/manifest/api_security_tester.tpl", path.module), {
     aws_s3_bucket_name    = indent(4, "")
     gcp_storage_bucket_name    = indent(4, "")

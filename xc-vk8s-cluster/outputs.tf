8b662d918c3a2eefc409c42d78e95cefcc2707a3
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-30 22:19:48 -0700

    Create outputs.tf

---
+++ xc-vk8s-cluster/outputs.tf
@@ -0,0 +1,8 @@
+output "xc_namespace" {
+  description = "XC namespace"
+  value       = format("https://oidc-provider.%s", var.app_domain)
+}
+output "xc_vk8s_kubeconfig" {
+  description = "Domain VIP to access the OIDC Proxy application, running on ADN"
+  value       = format("https://oidc-proxy.%s", var.app_domain)
+}

3bbb28d8ac639d728b384e63a3f608c27440a7d9
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-31 02:28:38 -0700

    Update outputs.tf

--- xc-vk8s-cluster/outputs.tf
+++ xc-vk8s-cluster/outputs.tf
@@ -24,7 +24,7 @@ output "xc_vk8s_host" {
 }
 
 output "xc_vk8s_ca_cert" {
-    value = yamldecode(base64decode(volterra_api_credential.this.data)).clusters[0].cluster.certificate-authority
+    value = yamldecode(base64decode(volterra_api_credential.this.data)).clusters[0].cluster.certificate-authority-data
 }
 
 output "xc_vk8s_client_cert" {

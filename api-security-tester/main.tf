6d9a91d0fd171e943986b44b55708d0255255cb0
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-31 03:33:40 -0700

    Update main.tf

--- api-security-tester/main.tf
+++ api-security-tester/main.tf
@@ -4,8 +4,8 @@ provider "volterra" {
 
   provider "kubectl" {
     host = data.tfe_outputs.xc-vk8s.values.xc_vk8s_host
-    cluster_ca_certificate = data.tfe_outputs.xc-vk8s.values.xc_vk8s_ca_cert
-    client_certificate = data.tfe_outputs.xc-vk8s.values.xc_vk8s_client_cert
-    client_key = data.tfe_outputs.xc-vk8s.values.xc_vk8s_client_key
+    cluster_ca_certificate = base64decode(data.tfe_outputs.xc-vk8s.values.xc_vk8s_ca_cert)
+    client_certificate = base64decode(data.tfe_outputs.xc-vk8s.values.xc_vk8s_client_cert)
+    client_key = base64decode(data.tfe_outputs.xc-vk8s.values.xc_vk8s_client_key)
     load_config_file       = false
 }

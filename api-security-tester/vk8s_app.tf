9f603fcf54108d5754a68c3eb52d5ac920de8ee5
vtobi <97698702+vtobi@users.noreply.github.com>
2023-03-31 22:31:42 -0700

    Update vk8s_app.tf

--- api-security-tester/vk8s_app.tf
+++ api-security-tester/vk8s_app.tf
@@ -36,30 +36,3 @@ resource "null_resource" "apply_manifest" {
     on_failure = continue
   }
 }
-
-
-#resource "kubectl_manifest" "apply_manifest" {
-#    depends_on = [time_sleep.wait_k8s_server]
-#    yaml_body = local.fdx_security_tester_manifest_content
-#}
-
-#resource "null_resource" "apply_manifest" {
-#  depends_on = [time_sleep.wait_k8s_server]
-#  triggers = {
-#    manifest_sha1 = sha1(local.fdx_security_tester_manifest_content)
-#  }
-#  provisioner "local-exec" {
-#    command = "kubectl apply -f _output/fdx-security-tester.yaml"
-#    environment = {
-#      KUBECONFIG = format("%s/_output/fdx_security_tester_adn_vk8s_kubeconfig", path.root)
-#    }
-#  }
-#  provisioner "local-exec" {
-#    when    = destroy
-#    command = "kubectl delete -f _output/fdx-security-tester.yaml --ignore-not-found=true"
-#    environment = {
-#      KUBECONFIG = format("%s/_output/fdx_security_tester_adn_vk8s_kubeconfig", path.root)
-#    }
-#    on_failure = continue
-#  }
-#}

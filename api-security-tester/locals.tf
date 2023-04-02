f436384a6188091b0b1b02ec40561832e203dab8
vtobi <97698702+vtobi@users.noreply.github.com>
2023-04-01 20:34:15 -0700

    Update locals.tf

--- api-security-tester/locals.tf
+++ api-security-tester/locals.tf
@@ -4,6 +4,12 @@ locals {
   fdx_security_tester_manifest_content = templatefile(format("%s/manifest/api_security_tester.tpl", path.module), {
     aws_s3_bucket_name    = indent(4, "")
     gcp_storage_bucket_name    = indent(4, "")
+    mail_host   = indent(4, var.mail_host)
+    mail_port    = indent(4, var.mail_port)
+    mail_api_key    = indent(4, var.mail_api_key)
+    mail_api_secret    = indent(4, var.mail_api_secret)
+    mail_from_addr    = indent(4, var.mail_from_addr)
+    mail_to_addr    = indent(4, var.mail_to_addr)   
   })
   js_delay_list = var.disable_js_challenge ? [] : [
     {
